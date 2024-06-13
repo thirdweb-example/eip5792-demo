e0b11918bcd1c721d27585aad78c6c37f47ea123
gregfromstl <gregfromstl@gmail.com>
2024-06-13 11:47:43 -0700

    augment headers

--- src/app/paymaster/route.ts
+++ src/app/paymaster/route.ts
@@ -7,14 +7,14 @@ export async function POST(request: NextRequest) {
   const clientId = searchParams.get("clientId");
   const data = await request.json();
   console.log("Request body", data);
+  const headers = request.headers;
+  headers.set("x-secret-key", process.env.SECRET_KEY!);
   const res = await fetch(
     `https://${chainId}.bundler.thirdweb.com/${clientId}`,
     {
       method: "POST",
       body: JSON.stringify(data),
-      headers: {
-        "x-secret-key": process.env.SECRET_KEY!,
-      },
+      headers,
     }
   );
   console.log("Paymaster result", res);
