ca1dc2f350083dcbec4954fb3e135125db5d6261
gregfromstl <gregfromstl@gmail.com>
2024-06-13 11:15:47 -0700

    logs

--- src/app/paymaster/route.ts
+++ src/app/paymaster/route.ts
@@ -1,10 +1,12 @@
 import { type NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
+  console.log("Request received", request);
   const searchParams = request.nextUrl.searchParams;
   const chainId = searchParams.get("chainId");
   const clientId = searchParams.get("clientId");
   const data = await request.json();
+  console.log("Request body", data);
   const res = await fetch(
     `https://${chainId}.bundler.thirdweb.com/${clientId}`,
     {
